522bbb85f1e823156036df85ad403c3e84e052d1
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-02-01 16:46:37 +0100

    base de datos indexedDB

---
+++ js/nuevocliente.js
@@ -0,0 +1,66 @@
+(function(){
+    let DB;
+    const formulario = document.querySelector('#formulario');
+
+    document.addEventListener('DOMContentLoaded',()=>{
+        conectarDB();
+        
+        formulario.addEventListener('submit', validarCliente);
+    });
+
+function validarCliente(e){
+    e.preventDefault();
+    
+    
+    // Leer los imputs
+    const nombre = document.querySelector('#nombre').value;
+    const email = document.querySelector('#email').value;
+    const telefono = document.querySelector('#telefono').value;
+    const empresa = document.querySelector('#empresa').value;
+   
+    if(nombre === ''||email === ''||telefono === ''||empresa === ''){
+        imprimirAlerta('Todos los campos son obligatorios','error');
+        return;
+    }
+   // Crear un objeto con la informacion
+
+   const cliente = {
+       nombre,        // esto es lo mismo que nombre : nombre 
+       email,
+       telefono,
+       empresa,
+       id : Date.now()
+   }
+ console.log(cliente)
+ 
+crearNuevoCliente(cliente);
+  
+ }
+ 
+function crearNuevoCliente(cliente){
+   const transaction = DB.transaction(['crm'], 'readwrite');
+
+   const objectStore = transaction.objectStore('crm');
+
+   objectStore.add(cliente);
+
+   transaction.onerror = ()=>{
+       imprimirAlerta('hubo un error(comprobar email) ', 'error');
+   }
+
+   transaction.oncomplete = ()=> {
+       console.log('añadido');
+       imprimirAlerta('Cliente añadido correctamente');
+       setTimeout(() => {
+           window.location.href ='index.html'
+       }, 3000);
+   }    
+   
+
+}
+
+
+
+   
+
+})();
